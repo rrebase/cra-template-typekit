22abdb503be1b7daa4b0967781bae67fd5813ac2
Ragnar Rebase <rrebase@gmail.com>
2020-01-31 09:52:36 +0200

    feat: remove React.FC type

--- template/src/App.tsx
+++ template/src/App.tsx
@@ -5,7 +5,7 @@ import Counter from "./features/counter/Counter";
 import { Provider } from "react-redux";
 import store from "./store";
 
-const App: React.FC = () => {
+const App = () => {
   return (
     <Provider store={store}>
       <div className="App">
